0b7fb0d14d63a1dc82119498017d4eac4dc3c582
Christian Joergensen <christian.joergensen@gmta.info>
2013-03-13 12:07:50 +0100

    Widget frontend rendering.

--- plugin/voxpress.php
+++ plugin/voxpress.php
@@ -58,7 +58,7 @@ class UbivoxAPI {
 
         if ($info["http_code"] == 200) {
 
-            $response = xmlrpc_decode($data);
+            $response = xmlrpc_decode($data, "utf-8");
 
             if (is_array($response) && xmlrpc_is_fault($response)) {
                 throw new UbivoxAPIError($response["faultString"], $response["faultCode"]);
@@ -233,12 +233,109 @@ class Ubivox_Widget extends WP_Widget {
 
         echo $args["before_widget"];
 
+        $title = apply_filters("widget_title", $instance["title"]);
+
+        if (!empty($title)) {
+            echo $args["before_title"].$title.$args["after_title"];
+        }
+
         echo '<form method="POST" class="ubivox_subscription">';
 
         echo '<p>';
-        echo '<label for="'.$this->get_field_id("email_address").'">E-mail</label><br>';
+        echo '<label for="'.$this->get_field_id("email_address").'">E-mail:</label><br>';
         echo '<input type="text" name="email_address" id="'.$this->get_field_id("email_address").'" value="">';
         echo '</p>';
+
+        foreach ($instance["data_meta"] as $field) {
+
+            if (!in_array($field["key"], $instance["data"])) {
+                continue;
+            }
+
+            switch ($field["datatype"]) {
+
+            case "textarea":
+
+                echo '<p>';
+                echo '<label for="'.$this->get_field_id("data_".$field["key"]).'">'.esc_html($field["title"]).':</label><br>';
+                echo '<textarea name="data_'.esc_attr($field["key"]).'" id="'.$this->get_field_id("data_".$field["key"]).'" style="width: 100%; height: 50px;"></textarea>';
+                echo '</p>';
+
+                break;
+
+            case "checkbox":
+
+                echo '<p>';
+                echo '<label for="'.$this->get_field_id("data_".$field["key"]).'">'.esc_html($field["title"]).':</label><br>';
+                echo '<input type="checkbox" name="data_'.esc_attr($field["key"]).'" id="'.$this->get_field_id("data_".$field["key"]).'" value="1">';
+                echo '</p>';
+
+                break;
+
+            case "select_multiple":
+            case "select":
+
+                if ($field["datatype"] == "select_multiple") {
+                    $multiple = " multiple";
+                } else {
+                    $multiple = "";
+                }
+
+                echo '<p>';
+                echo '<label for="'.$this->get_field_id("data_".$field["key"]).'">'.esc_html($field["title"]).':</label><br>';
+                echo '<select name="data_'.esc_attr($field["key"]).'" id="'.$this->get_field_id("data_".$field["key"]).'" style="width: 100%;"'.$multiple.'>';
+
+                foreach ($field["choices"] as $choice) {
+                    list($key, $value) = $choice;
+                    echo '<option value="'.esc_attr($key).'">'.esc_html($value).'</option>';
+                }
+
+                echo '</select>';
+
+                echo '</p>';
+
+                break;
+
+            case "select_radio":
+
+                echo '<p>';
+                echo '<label>'.esc_html($field["title"]).':</label><br>';
+
+                foreach ($field["choices"] as $choice) {
+
+                    list($key, $value) = $choice;
+                    echo '<input type="radio" name="data_'.esc_attr($field["key"]).'" id="'.$this->get_field_id("data_".$field["key"]."_".$key).'" value="'.esc_attr($key).'">&nbsp;';
+                    echo '<label for="'.$this->get_field_id("data_".$field["key"]."_".$key).'">'.esc_html($value).'</label><br>';
+                }
+
+                echo '</p>';
+
+                break;
+
+            case "select_checkbox":
+
+                echo '<p>';
+                echo '<label>'.esc_html($field["title"]).':</label><br>';
+
+                foreach ($field["choices"] as $choice) {
+
+                    list($key, $value) = $choice;
+                    echo '<input type="checkbox" name="data_'.esc_attr($field["key"]).'" id="'.$this->get_field_id("data_".$field["key"]."_".$key).'" value="'.esc_attr($key).'">&nbsp;';
+                    echo '<label for="'.$this->get_field_id("data_".$field["key"]."_".$key).'">'.esc_html($value).'</label><br>';
+                }
+
+                echo '</p>';
+
+            default:
+
+                echo '<p>';
+                echo '<label for="'.$this->get_field_id("data_".$field["key"]).'">'.esc_html($field["title"]).':</label><br>';
+                echo '<input type="text" name="data_'.esc_attr($field["key"]).'" id="'.$this->get_field_id("data_".$field["key"]).'" value="">';
+                echo '</p>';
+
+            }
+
+        }
 
         echo '<p>';
         echo '<button class="ubivox_signup">'.$instance["button_text"].'</button>';
@@ -250,16 +347,48 @@ class Ubivox_Widget extends WP_Widget {
     }
 
     public function update( $new_instance, $old_instance ) {
+
         $instance = array();
+
         $instance["list_id"] = intval($new_instance["list_id"]);
         $instance["button_text"] = strip_tags($new_instance["button_text"]);
+        $instance["title"] = strip_tags($new_instance["title"]);
+
+        try {
+            $api = new UbivoxAPI();
+            $data_fields = $api->call("ubivox.list_data_fields_details");
+        } catch (UbivoxAPIException $e) {
+            return $old_instance;
+        }
+
+        $data = Array();
+
+        foreach ($data_fields as $field) {
+            if (isset($new_instance["data_".$field["id"]])) {
+                $data[] = $field["key"];
+            }
+        }
+
+        $instance["data"] = $data;
+        $instance["data_meta"] = $data_fields;
+
         return $instance;
     }
 
     public function form( $instance ) {
 
-        $api = new UbivoxAPI();
-        $lists = $api->call("ubivox.get_maillists");
+        try {
+
+            $api = new UbivoxAPI();
+
+            $lists = $api->call("ubivox.get_maillists");
+            $data_fields = $api->call("ubivox.list_data_fields_details");
+
+        } catch (UbivoxAPIException $e) {
+            echo "<p>Connection problems. See settings for details.</p>";
+            return;
+        }
+
         $first_list = $lists[0];
             
         if (isset($instance["list_id"])) {
@@ -271,8 +400,25 @@ class Ubivox_Widget extends WP_Widget {
         if (isset($instance["button_text"])) {
             $button_text = $instance["button_text"];
         } else {
-            $title = __("Subscribe", "ubivox");
+            $button_text = __("Subscribe", "ubivox");
         }
+
+        if (isset($instance["title"])) {
+            $title = $instance["title"];
+        } else {
+            $title = __("Newsletter", "ubivox");
+        }
+
+        if (isset($instance["data"])) {
+            $data = $instance["data"];
+        } else {
+            $data = array();
+        }
+
+        echo '<p>';
+        echo '<label for="'.$this->get_field_id("title").'">Title:</label>';
+        echo '<input type="text" class="widefat" id="'.$this->get_field_id('title').'" name="'.$this->get_field_name('title').'" value="'.esc_attr($title).'">';
+        echo '</p>';
 
         echo '<p>';
         echo '<label for="'.$this->get_field_id("list_id").'">List:</label>';
@@ -291,6 +437,23 @@ class Ubivox_Widget extends WP_Widget {
         echo '<p>';
         echo '<label for="'.$this->get_field_id("button_text").'">Button text:</label>';
         echo '<input type="text" class="widefat" id="'.$this->get_field_id('button_text').'" name="'.$this->get_field_name('button_text').'" value="'.esc_attr($button_text).'">';
+        echo '</p>';
+
+        echo '<p>';
+        echo '<label>Data fields:</label><br>';
+
+        foreach ($data_fields as $field) {
+
+            if (in_array($field["key"], $data)) {
+                $checked = " checked";
+            } else {
+                $checked = "";
+            }
+
+            echo '<input class="checkbox" type="checkbox" id="'.$this->get_field_id('data_'.$field["id"]).'" name="'.$this->get_field_name('data_'.$field["id"]).'"'.$checked.'>&nbsp;';
+            echo '<label for="'.$this->get_field_id('data_'.$field["id"]).'">'.esc_html($field["title"]).' <em>('.esc_html($field["key"]).')</em></label><br>';
+        }
+
         echo '</p>';
 
     }

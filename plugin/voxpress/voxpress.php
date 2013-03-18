9d9807f79c72f073752a4869a9cd0ac163243668
Christian Joergensen <christian.joergensen@gmta.info>
2013-03-18 19:14:13 +0100

    Menu page.

--- plugin/voxpress/voxpress.php
+++ plugin/voxpress/voxpress.php
@@ -22,90 +22,12 @@ require "widgets/control_panel.php";  // Ubivox control panel widget
 
 require "woocommerce.php";            // Woocommerce integration
 
+require "admin/init.php";
+
 define(
     "UBIVOX_BASE_URL", get_option("uvx_api_url") ?
     preg_replace('#/xmlrpc/?$#', "", get_option("uvx_api_url")) : null
 );
-
-###############################################################################
-# Settings page
-###############################################################################
-
-add_action("admin_menu", "uvx_setup_menu");
-
-function uvx_setup_menu() {
-
-    add_menu_page(
-        "Ubivox", 
-        "Ubivox",
-        "manage_options",
-        "voxpress",
-        "uvx_page",
-        null,
-        30
-    );
-
-    add_submenu_page(
-        "voxpress", 
-        "Ubivox Overview",
-        "Overview", 
-        "manage_options", 
-        "voxpress", 
-        "uvx_page"
-    );
-
-    add_submenu_page(
-        "voxpress", 
-        "Ubivox Options",
-        "Options", 
-        "manage_options", 
-        "voxpress-options", 
-        "uvx_options_page"
-    );
-
-    add_submenu_page(
-        "voxpress", 
-        "Ubivox Statistics",
-        "Statistics", 
-        "manage_options", 
-        "voxpress-statistics", 
-        "uvx_statistics_page"
-    );
-
-    add_submenu_page(
-        "voxpress", 
-        "Ubivox WooCommerce Integration",
-        "WooCommerce", 
-        "manage_options", 
-        "voxpress-woocommerce", 
-        "uvx_woocommerce_page"
-    );
-
-}
-
-function uvx_page() {
-
-    if (!current_user_can("manage_options")) {
-        wp_die(__(
-            "You do not have sufficient permissions to access this page."
-        ));
-    }
-
-    require "admin/overview.php";
-
-}
-
-function uvx_options_page() {
-
-    if (!current_user_can("manage_options")) {
-        wp_die(__(
-            "You do not have sufficient permissions to access this page."
-        ));
-    }
-
-    require "admin/settings.php";
-
-}
 
 ###############################################################################
 # Widgets

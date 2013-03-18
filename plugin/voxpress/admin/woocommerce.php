aa953babd40877b1fc3ccb18b9dc1d999455859b
Christian Joergensen <christian.joergensen@gmta.info>
2013-03-18 20:34:47 +0100

    Oops.

---
+++ plugin/voxpress/admin/woocommerce.php
@@ -0,0 +1,16 @@
+<div class="wrap">
+
+<?php screen_icon(); ?>
+<h2>WooCommerce Integration</h2>
+
+<form method="post">
+
+<?php settings_fields("ubivox-woocommerce-group"); ?>
+
+<?php do_settings_fields("ubivox-woocommerce-group", "main"); ?>
+
+<?php submit_button(); ?>
+
+</form>
+
+</div>

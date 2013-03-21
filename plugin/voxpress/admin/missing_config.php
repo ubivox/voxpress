2e1e30eb59a633e5d7d29e882227439e31179ab9
Christian Joergensen <christian.joergensen@gmta.info>
2013-03-21 23:00:28 +0100

    Optimized archive access.

---
+++ plugin/voxpress/admin/missing_config.php
@@ -0,0 +1,9 @@
+<div class="wrap">
+
+     <h2>The Ubivox plugin is not configured yet</h2>
+
+     <p>
+         <a href="<?php echo admin_url("/admin.php?page=voxpress-options"); ?>">Configure the Ubivox plugin</a>
+     </p>
+
+</div>

5dd008cb0c7456d0dff6bed6c3a396aa9436304e
Christian Joergensen <christian@technobabble.dk>
2013-04-11 15:08:12 +0200

    Label, intial state support.

--- plugin/voxpress/admin/ecommerce.php
+++ plugin/voxpress/admin/ecommerce.php
@@ -5,6 +5,8 @@ $saved = false;
 if (isset($_POST["_save"])) {
     update_option("ubivox_ecommerce_maillist_id", intval($_POST["maillist_id"]));
     update_option("ubivox_ecommerce_target_id", intval($_POST["target_id"]));
+    update_option("ubivox_ecommerce_subscribe_initial", intval($_POST["subscribe_initial"]));
+    update_option("ubivox_ecommerce_subscribe_label", $_POST["subscribe_label"]);
     $saved = true;
 }
 
@@ -19,6 +21,8 @@ try {
 
 $maillist_id = get_option("ubivox_ecommerce_maillist_id", 0);
 $target_id = get_option("ubivox_ecommerce_target_id", 0);
+$subscribe_label = get_option("ubivox_ecommerce_subscribe_label", "Subscribe to our newsletter?");
+$subscribe_initial = get_option("ubivox_ecommerce_subscribe_initial", 0);
 
 ?>
 
@@ -41,7 +45,7 @@ $target_id = get_option("ubivox_ecommerce_target_id", 0);
     <label for="id_maillist_id">Add checkbox for:</label>
   </th>
   <td>
-    <select id="id_maillist_id" name="maillist_id">
+    <select id="id_maillist_id" name="maillist_id" style="width: 400px;">
     <option value="0"<?php echo $maillist_id == 0 ? " selected" : ""?>>- Do not use -</option>
       <?php foreach ($maillists as $maillist): ?>
       <option value="<?php echo esc_attr($maillist["id"]); ?>"<?php echo $maillist_id == $maillist["id"] ? " selected" : ""?>><?php echo esc_html($maillist["title"]); ?></option>
@@ -51,10 +55,33 @@ $target_id = get_option("ubivox_ecommerce_target_id", 0);
 </tr>
 <tr valign="top">
   <th>
+    <label for="id_subscribe_label">Checkbox label:</label>
+  </th>
+  <td>
+    <input type="text" id="id_subscribe_label" name="subscribe_label" value="<?php echo esc_attr($subscribe_label); ?>" style="width: 400px;">
+  </td>
+</tr>
+<tr valign="top">
+  <th>
+    <label>Initial state:</label>
+  </th>
+  <td>
+    <input type="radio" id="id_subscribe_initial_checked" name="subscribe_initial" value="0"<?php echo $subscribe_initial == 0 ? " checked" : ""; ?>>&nbsp;<label for="id_subscribe_initial_checked">Unhecked</label><br>
+    <input type="radio" id="id_subscribe_initial_unchecked" name="subscribe_initial" value="1"<?php echo $subscribe_initial == 1 ? " checked" : ""; ?>>&nbsp;<label for="id_subscribe_initial_unchecked">Checked</label>
+
+  </td>
+</tr>
+</table>
+
+<h3>Sales tracking</h3>
+
+<table class="form-table">
+<tr valign="top">
+  <th>
     <label for="id_target_id">Add sales tracking target:</label>
   </th>
   <td>
-    <select id="id_target_id" name="target_id">
+    <select id="id_target_id" name="target_id" style="width: 400px;">
     <option value="0"<?php echo $target_id == 0 ? " selected" : ""?>>- Do not use -</option>
       <?php foreach ($targets as $target): ?>
       <option value="<?php echo esc_attr($target["id"]); ?>"<?php echo $target_id == $target["id"] ? " selected" : ""?>><?php echo esc_html($target["title"]); ?></option>
